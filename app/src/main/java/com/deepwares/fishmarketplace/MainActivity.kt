c7fc5ab9ce6d0a4318736be4ee28b9f4a2eab003
Deepak Sundar <deepak@airtime.com>
2020-06-12 07:58:17 +0530

    fix navigation issue

--- app/src/main/java/com/deepwares/fishmarketplace/MainActivity.kt
+++ app/src/main/java/com/deepwares/fishmarketplace/MainActivity.kt
@@ -5,8 +5,10 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,13 +16,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        // setSupportActionBar(toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_existing, R.id.navigation_new, R.id.navigation_past
+            )
+        )
+
+        setSupportActionBar(toolbar)
         setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
+       // bottomNav.setupWithNavController(navController)
+
+
+/*
+        NavigationUI.setupWithNavController(
+            toolbar, navController,
+            // appBarConfiguration
+            AppBarConfiguration.Builder(
+                setOf(
+                    R.id.navigation_existing, R.id.navigation_new, R.id.navigation_past
+                )
+            ).build()
+        )
+
+ */
+       // NavigationUI.setupWithNavController(nav_view, navController)
+        //setupActionBarWithNavController(toolbar, navController, appBarConfiguration)
+        // navView.setupWithNavController(navController)
     }
 }
